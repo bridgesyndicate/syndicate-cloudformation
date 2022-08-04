f1dc4bb4ee128ce81c454af96c1f9492b819fb92
Kenneth Berland <ken@hero.net>
2022-08-04 14:02:23 -0700

    move app config to us-west-2

--- update-stack.sh
+++ update-stack.sh
@@ -1,3 +1,3 @@
 aws cloudformation update-stack --region us-west-2 --stack-name SyndicateVPCAndBroker     --template-body file://syndicate-vpc-and-broker.yaml
 aws cloudformation update-stack --region us-east-2 --stack-name SyndicateVPCAndContainers --template-body file://us-east-2.yaml
-aws cloudformation update-stack --region us-east-2 --stack-name SyndicateGameAutoScaler   --template-body file://app-config.yaml
+aws cloudformation update-stack --region us-west-2 --stack-name SyndicateGameAutoScaler   --template-body file://app-config.yaml
